117634788ee3716a284ecb47bce5db4063343e36
anhthu258 <k.anhthu02@gmail.com>
2025-12-15 19:06:24 +0100

    Revert "pages..."
    This reverts commit a9f753a85912d8c1019a459a0637dcf23fc4143b.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: "/"
-  };
-
-  if (command !== "serve") {
-    config.base = "/godsbanen/";
-  }
-
-  return config;
-});
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/godsbanen/',
+  plugins: [react(), tailwindcss()],
+})
