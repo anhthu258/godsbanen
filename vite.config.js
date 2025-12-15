a9f753a85912d8c1019a459a0637dcf23fc4143b
anhthu258 <k.anhthu02@gmail.com>
2025-12-15 19:02:04 +0100

    pages...

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/godsbanen/',
-  plugins: [react(), tailwindcss()],
-})
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/"
+  };
+
+  if (command !== "serve") {
+    config.base = "/godsbanen/";
+  }
+
+  return config;
+});
