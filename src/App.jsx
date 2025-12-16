589c37bb5a29c8328fee96a222cd5d604fd9ed96
W1ngdings <emil35126@hotmail.com>
2025-12-16 07:09:53 +0100

    commmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router";
-import Workshop from './pages/Workshop';
-import Events from './pages/Events';
-import Home from './pages/Home';
-import Error from './pages/Error';
-import Keramik from './pages/keramik';
-import Booking from './pages/Booking';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Workshop from "./pages/Workshop";
+import Events from "./pages/Events";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import Keramik from "./pages/Keramik";
+import Booking from "./pages/Booking";
 
-function App() { 
-
+function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           {/*<Route path="/" element={<Hometest />} />*/}
-          <Route path="/" element={<Home/>} />
-          <Route path="/workshop" element={<Workshop/>} />
-          <Route path="/events" element={<Events/>} />
-          <Route path="/keramik" element={<Keramik/>} />
-          <Route path="/booking" element={<Booking/>} />
-          <Route path="*" element={<Error/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/workshop" element={<Workshop />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/keramik" element={<Keramik />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </BrowserRouter>
     </>
   );
 }
 
-export default App
+export default App;
