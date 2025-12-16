4c699bb89af867c92528d26480ba5bc93b71a5eb
anhthu258 <k.anhthu02@gmail.com>
2025-12-16 10:30:18 +0100

    nye page forsøgt

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
-    base: command === "serve" ? "/" : "/godsbanen/", // 👈 Replace with your GitHub repository name
+    base: command === "serve" ? "/" : "/godsbanen/"
   };
 });
